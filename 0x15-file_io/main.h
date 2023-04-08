d0956b5413252c01683b3189e622e7ef99eb25d6
GodSon4705 <olaleyeseun892@gmail.com>
2023-04-08 10:39:05 -0700

    100-elf_header.c

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,5 +1,5 @@
-#ifndef _MAIN_H
-#define _MAIN_H
+#ifndef _MAIN_H_
+#define _MAIN_H_
 
 #include <sys/types.h>
 #include <sys/stat.h>
@@ -10,10 +10,8 @@
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
-void check_sys(char *ptr)
-void print_magic(char *ptr)
-void print_data(char *ptr)
-int check_elf(char *ptr)
-int main(int argc, char *argv[])
-
+int check_elf(char *ptr);
+int main(int argc, char *argv[]);
+void check_sys(char *ptr);
+void print_magic(char *ptr);
 #endif
