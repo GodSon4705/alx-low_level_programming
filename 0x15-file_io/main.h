0714fd1815df5a431b3d4e4e75a5d7710cac3500
GodSon4705 <olaleyeseun892@gmail.com>
2023-04-08 09:47:20 -0700

    0x15-file_io

---
+++ 0x15-file_io/main.h
@@ -0,0 +1,19 @@
+#ifndef _MAIN_H
+#define _MAIN_H
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+ssize_t read_textfile(const char *filename, size_t letters);
+int create_file(const char *filename, char *text_content);
+int append_text_to_file(const char *filename, char *text_content);
+void check_sys(char *ptr)
+void print_magic(char *ptr)
+void print_data(char *ptr)
+int check_elf(char *ptr)
+int main(int argc, char *argv[])
+
+#endif
